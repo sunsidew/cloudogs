8b4e4d502c9b40942cb4f5b73a6f80b8e92e28d6
sunsidew <danielay@naver.com>
2014-06-10 19:57:38 +0900

    m:n mapping, user_docs_list making

---
+++ db/migrate/20140610105244_docs_users.rb
@@ -0,0 +1,4 @@
+class DocsUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610101025) do
+ActiveRecord::Schema.define(version: 20140610105244) do
 
   create_table "dialogs", force: true do |t|
     t.integer  "docs_id"
